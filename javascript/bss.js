d2745fa585261861ab8e364cf7d0a0be2825774b
Thophles <twilli16@saic.edu>
2017-12-26 21:41:00 -0600

    Added a project page, enhanced About Page, adjusted menu on home page

---
+++ javascript/bss.js
@@ -0,0 +1,8 @@
+$(document).on('click', 'a[href^="#feat"]', function (event) {
+	"use strict";
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+});
